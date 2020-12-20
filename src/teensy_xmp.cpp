22a3f7b14bd3e1a39434c2682a5b44f05a9c5310
jcj83429 <jcj83429@gmail.com>
2020-12-19 18:32:32 -0800

    do the module rendering in the background
    same technique as Frank Boesing's Arduino-Teensy-Codec-lib
    
    no more stuttering at 24MHz

--- src/teensy_xmp.cpp
+++ src/teensy_xmp.cpp
@@ -1,5 +1,19 @@
 #include "teensy_xmp.h"
 
+// using the same interrupt as the Audio Codecs library
+#define IRQ_AUDIO			IRQ_SOFTWARE	// see AudioStream.cpp
+#if defined(__IMXRT1052__) && !defined(__IMXRT1062__)
+#define IRQ_AUDIOCODEC		IRQ_Reserved1
+#else
+#define IRQ_AUDIOCODEC		55				// use a "reserved" (free) interrupt vector
+#endif
+
+#define IRQ_AUDIOCODEC_PRIO	240				// lowest priority
+
+#define NVIC_STIR			(*(volatile uint32_t *)0xE000EF00) //Software Trigger Interrupt Register
+#define NVIC_TRIGGER_INTERRUPT(x)    NVIC_STIR=(x)
+#define NVIC_IS_ACTIVE(n)	(*((volatile uint32_t *)0xE000E300 + ((n) >> 5)) & (1 << ((n) & 31)))
+
 void __attribute__((weak)) *xmp_malloc(size_t size){
     return malloc(size);
 }
@@ -16,40 +30,92 @@ void *xmp_calloc(size_t n, size_t size){
     return mem;
 }
 
+TeensyXmp *playingTeensyXmp = NULL;
+void decodeModule(void);
+
 bool TeensyXmp::playModuleInMemory(void *buf, int size){
     if(playState != TeensyXmpState::STOP){
         stop();
     }
+    
+    decodeBuf[0] = (int16_t*)malloc(sizeof(int16_t) * 2 * TEENSY_XMP_BUF_SMPS);
+    if(decodeBuf[0] == NULL){
+        Serial.println("playModuleInMemory failed to allocate decodeBuf");
+        return false;
+    }
+
+    decodeBuf[1] = (int16_t*)malloc(sizeof(int16_t) * 2 * TEENSY_XMP_BUF_SMPS);
+    if(decodeBuf[0] == NULL){
+        Serial.println("playModuleInMemory failed to allocate decodeBuf");
+        goto fail1;
+    }
+    
     int err;
     xmpctx = xmp_create_context();
+    if(xmpctx == NULL){
+        Serial.print("xmp_create_context returned null");
+        goto fail2;
+    }
     err = xmp_load_module_from_memory(xmpctx, buf, size);
     if(err){
         Serial.print("xmp_load_module_from_memory failed with error ");
         Serial.println(err);
-        goto fail1;
+        goto fail3;
     }
     err = xmp_start_player(xmpctx, 44100, 0);
     if(err){
         Serial.print("xmp_start_player failed with error ");
         Serial.println(err);
-        goto fail2;
+        goto fail4;
     }
+    decodeBufSmps[0] = decodeBufSmps[1] = 0;
+    playingBuf = 0;
+    lastErr = 0;
+
+    playingTeensyXmp = this;
     playState = TeensyXmpState::PLAY;
+    //decodeModule();
+    if(playState != TeensyXmpState::PLAY){
+        // something went wrong in decodeModule
+        return false;
+    }
+
+    attachInterruptVector((IRQ_NUMBER_t)IRQ_AUDIOCODEC, decodeModule);
+    if(NVIC_GET_PRIORITY(IRQ_AUDIO) >= IRQ_AUDIOCODEC_PRIO){
+        NVIC_SET_PRIORITY(IRQ_AUDIO, IRQ_AUDIOCODEC_PRIO-16);
+    }
+    NVIC_SET_PRIORITY(IRQ_AUDIOCODEC, IRQ_AUDIOCODEC_PRIO);
+    NVIC_ENABLE_IRQ(IRQ_AUDIOCODEC);
+
     return true;
     
-fail2:
+fail4:
     xmp_release_module(xmpctx);
-fail1:
+fail3:
     xmp_free_context(xmpctx);
+fail2:
+    free(decodeBuf[1]);
+fail1:
+    free(decodeBuf[0]);
     return false;
 }
 
 void TeensyXmp::stop(){
+    NVIC_DISABLE_IRQ(IRQ_AUDIOCODEC);
+    playingTeensyXmp = NULL;
     if(playState != TeensyXmpState::STOP){
         playState = TeensyXmpState::STOP;
         xmp_end_player(xmpctx);
         xmp_release_module(xmpctx);
         xmp_free_context(xmpctx);
+    }
+    if(decodeBuf[1]){
+        free(decodeBuf[1]);
+        decodeBuf[1] = NULL;
+    }
+    if(decodeBuf[0]){
+        free(decodeBuf[0]);
+        decodeBuf[0] = NULL;
     }
     posMs = 0;
 }
@@ -58,36 +124,73 @@ void TeensyXmp::update(){
     if(playState != TeensyXmpState::PLAY){
         return;
     }
+
+    // chain the decode interrupt
+
+    if (!NVIC_IS_ACTIVE(IRQ_AUDIOCODEC)){
+        NVIC_TRIGGER_INTERRUPT(IRQ_AUDIOCODEC);
+    }
+
+    if(!playingTeensyXmp->decodeBufSmps[playingBuf]){
+        if(playingTeensyXmp->decodeBufSmps[1 - playingBuf]){
+            // other buffer is ready
+            playingBuf = 1 - playingBuf;
+        }else{
+            // buffer underrun!
+            return;
+        }
+    }
+    
+    int16_t *pb = playingTeensyXmp->decodeBuf[playingBuf] + 2 * (TEENSY_XMP_BUF_SMPS - playingTeensyXmp->decodeBufSmps[playingBuf]);
+
     audio_block_t	*block_left = allocate();
     if(!block_left){
         return;
     }
     audio_block_t	*block_right = allocate();
     if(!block_right){
+        release(block_left);
         return;
     }
-    
-    int16_t playBuf[256]; // 128 samples per channel
-    int err = xmp_play_buffer(xmpctx, playBuf, sizeof(playBuf), 1);
-
     for(int i = 0, j = 0; i < 128; i++){
-        block_left->data[i] = playBuf[j++];
-        block_right->data[i] = playBuf[j++];
+        block_left->data[i] = pb[j++];
+        block_right->data[i] = pb[j++];
     }
     transmit(block_left, 0);
     transmit(block_right, 1);
     release(block_left);
     release(block_right);
+    
+    playingTeensyXmp->decodeBufSmps[playingBuf] -= 128;
+}
 
+void decodeModule(void){
+    if(!playingTeensyXmp){
+        return;
+    }
+    if(playingTeensyXmp->playState != TeensyXmpState::PLAY){
+        return;
+    }
+    int decodingBuf = 1 - playingTeensyXmp->playingBuf;
+    if(playingTeensyXmp->decodeBufSmps[decodingBuf]){
+        // nothing to do. wait until update() switches playingBuf.
+        // we never partially fill a decode buffer.
+        return;
+    }
+
+    int err = xmp_play_buffer(playingTeensyXmp->xmpctx, playingTeensyXmp->decodeBuf[decodingBuf], TEENSY_XMP_BUF_BYTES, 1);
     if(err){
         if(err != -XMP_END){
             Serial.print("xmp_play_buffer returned error ");
             Serial.println(err);
         }
-        stop();
+        playingTeensyXmp->lastErr = err;
+        playingTeensyXmp->stop();
     }else{
         struct xmp_frame_info fi;
-        xmp_get_frame_info(xmpctx, &fi);
-        posMs = fi.time;
+        xmp_get_frame_info(playingTeensyXmp->xmpctx, &fi);
+        playingTeensyXmp->posMs = fi.time;
+        playingTeensyXmp->decodeBufSmps[decodingBuf] = TEENSY_XMP_BUF_SMPS;
+        // now update() may use the new buffer
     }
 }
--- src/teensy_xmp.h
+++ src/teensy_xmp.h
@@ -1,9 +1,16 @@
 #ifndef TEENSY_XMP_H
 #define TEENSH_XMP_H
 
+#if !defined(__MK20DX256__) && !defined(__MK64FX512__) && !defined(__MK66FX1M0__) && !defined(__IMXRT1052__) && !defined(__IMXRT1062__)
+#error	This platform is not supported.
+#endif
+
 #include "Arduino.h"
 #include "AudioStream.h"
 #include "xmp.h"
+
+#define TEENSY_XMP_BUF_SMPS 1024
+#define TEENSY_XMP_BUF_BYTES (TEENSY_XMP_BUF_SMPS * 2 * sizeof(int16_t))
 
 enum TeensyXmpState {STOP, PLAY, PAUSE};
 
@@ -13,6 +20,9 @@ public:
     TeensyXmp(void) : AudioStream(0, NULL){
         playState = TeensyXmpState::STOP;
         posMs = 0;
+        decodeBuf[0] = decodeBuf[1] = NULL;
+        decodeBufSmps[0] = decodeBufSmps[1] = 0;
+        playingBuf = 0;
     }
 
     bool playModuleInMemory(void *buf, int size);
@@ -25,10 +35,18 @@ public:
     }
 
     void update(void);
+    
+    int lastErr;
 private:
     xmp_context xmpctx;
     TeensyXmpState playState;
     int posMs;
+    int16_t *decodeBuf[2];
+    // samples remaining in decodeBuf[i], each sample is 4 bytes (int16_t * 2)
+    int decodeBufSmps[2];
+    int playingBuf;
+    
+    friend void decodeModule(void);
 };
 
 #endif
