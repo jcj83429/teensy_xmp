jcj83429/teensy_xmp

commit 6e60d8c79dda671b8b2c4b7097a82a24d0e3ba55

add callback interface to support loading module directly from file

src/hio.c
@@ -62,6 +62,13 @@ int8 hio_read8s(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread8s(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &ret, 1, 1);
+		if (bytes_read != 1) {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -82,6 +89,13 @@ uint8 hio_read8(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread8(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &ret, 1, 1);
+		if (bytes_read != 1) {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -102,6 +116,16 @@ uint16 hio_read16l(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread16l(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[2];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 2);
+		if (bytes_read == 2) {
+			ret = tmp[1] << 8 | tmp[0];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -122,6 +146,16 @@ uint16 hio_read16b(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread16b(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[2];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 2);
+		if (bytes_read == 2) {
+			ret = tmp[0] << 8 | tmp[1];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -142,6 +176,16 @@ uint32 hio_read24l(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread24l(h->handle.mem); 
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[3];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 3);
+		if (bytes_read == 3) {
+			ret = tmp[2] << 16 | tmp[1] << 8 | tmp[0];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -162,6 +206,16 @@ uint32 hio_read24b(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread24b(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[3];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 3);
+		if (bytes_read == 3) {
+			ret = tmp[0] << 16 | tmp[1] << 8 | tmp[2];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -182,6 +236,16 @@ uint32 hio_read32l(HIO_HANDLE *h)
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread32l(h->handle.mem);
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[4];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 4);
+		if (bytes_read == 4) {
+			ret = tmp[3] << 24 | tmp[2] << 16 | tmp[1] << 8 | tmp[0];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -201,6 +265,16 @@ uint32 hio_read32b(HIO_HANDLE *h)
 		break;
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread32b(h->handle.mem);
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		uint8 tmp[4];
+		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 4);
+		if (bytes_read == 4) {
+			ret = tmp[0] << 24 | tmp[1] << 16 | tmp[2] << 8 | tmp[3];
+		} else {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -227,6 +301,13 @@ size_t hio_read(void *buf, size_t size, size_t num, HIO_HANDLE *h)
 			h->error = errno;
 		}
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		ret = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, buf, size, num);
+		if (ret != num) {
+			h->error = EOF;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -249,6 +330,13 @@ int hio_seek(HIO_HANDLE *h, long offset, int whence)
 			h->error = errno;
 		}
 		break;
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		ret = (* h->handle.callbacks->seek)(h->handle.callbacks->user_data, offset, whence);
+		if (ret < 0) {
+			h->error = ret;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -265,12 +353,13 @@ long hio_tell(HIO_HANDLE *h)
 			h->error = errno;
 		}
 		break;
-	case HIO_HANDLE_TYPE_MEMORY:
-		ret = mtell(h->handle.mem);
+	case HIO_HANDLE_TYPE_CALLBACKS: {
+		ret = (* h->handle.callbacks->tell)(h->handle.callbacks->user_data);
 		if (ret < 0) {
-			h->error = errno;
+			h->error = ret;
 		}
 		break;
+	}
 	}
 
 	return ret;
@@ -283,6 +372,8 @@ int hio_eof(HIO_HANDLE *h)
 		return feof(h->handle.file);
 	case HIO_HANDLE_TYPE_MEMORY:
 		return meof(h->handle.mem);
+	case HIO_HANDLE_TYPE_CALLBACKS:
+		return (* h->handle.callbacks->eof)(h->handle.callbacks->user_data);
 	default:
 		return EOF;
 	}
@@ -355,6 +446,43 @@ HIO_HANDLE *hio_open_file(FILE *f)
 	return h;
 }
 
+HIO_HANDLE *hio_open_callbacks(struct xmp_io_callbacks *cb)
+{
+	if (cb == NULL) {
+		return NULL;
+	}
+	
+	HIO_HANDLE *h;
+	h = (HIO_HANDLE *)xmp_malloc(sizeof (HIO_HANDLE));
+	if (h == NULL)
+		return NULL;
+
+	h->error = 0;
+	h->type = HIO_HANDLE_TYPE_CALLBACKS;
+	h->handle.callbacks = cb;
+	if (cb->size != NULL) {
+		h->size = (* cb->size)(cb->user_data);
+	} else {
+		int error = (* cb->seek)(cb->user_data, 0, SEEK_END);
+		if (error) {
+			goto fail;
+		}
+		h->size = (* cb->tell)(cb->user_data);
+		if (h->size < 0) {
+			goto fail;
+		}
+		error = (* cb->seek)(cb->user_data, 0, SEEK_SET);
+		if (error) {
+			goto fail;
+		}
+	}
+
+	return h;
+fail:
+	xmp_free(h);
+	return NULL;
+}
+
 int hio_close(HIO_HANDLE *h)
 {
 	int ret;
@@ -365,6 +493,9 @@ int hio_close(HIO_HANDLE *h)
 		break;
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mclose(h->handle.mem);
+		break;
+	case HIO_HANDLE_TYPE_CALLBACKS:
+		ret = 0;
 		break;
 	default:
 		ret = -1;

src/hio.h
@@ -5,17 +5,20 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include "memio.h"
+#include "xmp.h"
 
 #define HIO_HANDLE_TYPE(x) ((x)->type)
 
 typedef struct {
 #define HIO_HANDLE_TYPE_FILE	0
 #define HIO_HANDLE_TYPE_MEMORY	1
+#define HIO_HANDLE_TYPE_CALLBACKS	2
 	int type;
 	long size;
 	union {
 		FILE *file;
 		MFILE *mem;
+		struct xmp_io_callbacks *callbacks;
 	} handle;
 	int error;
 } HIO_HANDLE;
@@ -36,6 +39,7 @@ int	hio_error	(HIO_HANDLE *);
 HIO_HANDLE *hio_open	(void *, char *);
 HIO_HANDLE *hio_open_mem  (void *, long);
 HIO_HANDLE *hio_open_file (FILE *);
+HIO_HANDLE *hio_open_callbacks(struct xmp_io_callbacks *cb);
 int	hio_close	(HIO_HANDLE *);
 long	hio_size	(HIO_HANDLE *);
 

src/load.c
@@ -587,6 +587,31 @@ int xmp_load_module_from_file(xmp_context opaque, void *file, long size)
 	return ret;
 }
 
+int xmp_load_module_from_callbacks(xmp_context opaque, struct xmp_io_callbacks *cb)
+{
+	struct context_data *ctx = (struct context_data *)opaque;
+	struct module_data *m = &ctx->m;
+	int ret;
+	HIO_HANDLE *h = hio_open_callbacks(cb);
+	
+	if (h == NULL)
+		return -XMP_ERROR_SYSTEM;
+	
+	if (ctx->state > XMP_STATE_UNLOADED)
+		xmp_release_module(opaque);
+	
+	m->filename = NULL;
+	m->basename = NULL;
+	m->dirname = NULL;
+	m->size = hio_size(h);
+	
+	ret = load_module(opaque, h);
+
+	hio_close(h);
+
+	return ret;
+}
+
 void xmp_release_module(xmp_context opaque)
 {
 	struct context_data *ctx = (struct context_data *)opaque;

src/teensy_xmp.cpp
@@ -74,7 +74,74 @@ bool TeensyXmp::playModuleInMemory(void *buf, int size){
 
     playingTeensyXmp = this;
     playState = TeensyXmpState::PLAY;
-    //decodeModule();
+    decodeModule();
+    if(playState != TeensyXmpState::PLAY){
+        // something went wrong in decodeModule
+        return false;
+    }
+
+    attachInterruptVector((IRQ_NUMBER_t)IRQ_AUDIOCODEC, decodeModule);
+    if(NVIC_GET_PRIORITY(IRQ_AUDIO) >= IRQ_AUDIOCODEC_PRIO){
+        NVIC_SET_PRIORITY(IRQ_AUDIO, IRQ_AUDIOCODEC_PRIO-16);
+    }
+    NVIC_SET_PRIORITY(IRQ_AUDIOCODEC, IRQ_AUDIOCODEC_PRIO);
+    NVIC_ENABLE_IRQ(IRQ_AUDIOCODEC);
+
+    return true;
+    
+fail4:
+    xmp_release_module(xmpctx);
+fail3:
+    xmp_free_context(xmpctx);
+fail2:
+    free(decodeBuf[1]);
+fail1:
+    free(decodeBuf[0]);
+    return false;
+}
+
+bool TeensyXmp::playModuleWithCallbacks(struct xmp_io_callbacks *cb){
+    if(playState != TeensyXmpState::STOP){
+        stop();
+    }
+    
+    decodeBuf[0] = (int16_t*)malloc(sizeof(int16_t) * 2 * TEENSY_XMP_BUF_SMPS);
+    if(decodeBuf[0] == NULL){
+        Serial.println("playModuleInMemory failed to allocate decodeBuf");
+        return false;
+    }
+
+    decodeBuf[1] = (int16_t*)malloc(sizeof(int16_t) * 2 * TEENSY_XMP_BUF_SMPS);
+    if(decodeBuf[0] == NULL){
+        Serial.println("playModuleInMemory failed to allocate decodeBuf");
+        goto fail1;
+    }
+    
+    int err;
+    xmpctx = xmp_create_context();
+    if(xmpctx == NULL){
+        Serial.print("xmp_create_context returned null");
+        goto fail2;
+    }
+    err = xmp_load_module_from_callbacks(xmpctx, cb);
+    if(err){
+        Serial.print("xmp_load_module_from_callbacks failed with error ");
+        Serial.println(err);
+        goto fail3;
+    }
+    err = xmp_start_player(xmpctx, 44100, 0);
+    if(err){
+        Serial.print("xmp_start_player failed with error ");
+        Serial.println(err);
+        goto fail4;
+    }
+    decodeBufSmps[0] = decodeBufSmps[1] = 0;
+    playingBuf = 0;
+    lastErr = 0;
+
+    playingTeensyXmp = this;
+    playState = TeensyXmpState::PLAY;
+    decodeModule();
     if(playState != TeensyXmpState::PLAY){
         // something went wrong in decodeModule
         return false;

src/teensy_xmp.h
@@ -26,6 +26,7 @@ public:
     }
 
     bool playModuleInMemory(void *buf, int size);
+	bool playModuleWithCallbacks(struct xmp_io_callbacks *cb);
     void stop(void);
     bool isPlaying(void){
         return playState != TeensyXmpState::STOP;

src/xmp.h
@@ -310,6 +310,20 @@ struct xmp_frame_info {			/* Current frame information */
 	} channel_info[XMP_MAX_CHANNELS];
 };
 
+struct xmp_io_callbacks {
+	// return the number of elements read. Each element is of size "size"
+	size_t (*read)(void *user_data, void *buf, size_t size, size_t num);
+	// return 0 if seek was successful. Return negative if seek failed.
+	int (*seek)(void *user_data, long offset, int whence);
+	// return current offset, or negative for error
+	long (*tell)(void *user_data);
+	// return non-zero if eof
+    int (*eof)(void *user_data);
+	// size() can be set to NULL. 
+	// In that case, the seek implementation must support SEEK_END
+	long (*size)(void *user_data);
+	void *user_data;
+};
 
 typedef char *xmp_context;
 
@@ -343,6 +357,7 @@ EXPORT int         xmp_get_player      (xmp_context, int);
 EXPORT int         xmp_set_instrument_path (xmp_context, char *);
 EXPORT int         xmp_load_module_from_memory (xmp_context, void *, long);
 EXPORT int         xmp_load_module_from_file (xmp_context, void *, long);
+EXPORT int         xmp_load_module_from_callbacks (xmp_context, struct xmp_io_callbacks *cb);
 
 /* External sample mixer API */
 EXPORT int         xmp_start_smix       (xmp_context, int, int);

commit c553941645795adaed5553253ec0e410f3176d17

fix hio_tell and hio_read32b for HIO_HANDLE_TYPE_MEMORY
broken by 6e60d8c79dda671b8b2c4b7097a82a24d0e3ba55

src/hio.c
@@ -265,6 +265,7 @@ uint32 hio_read32b(HIO_HANDLE *h)
 		break;
 	case HIO_HANDLE_TYPE_MEMORY:
 		ret = mread32b(h->handle.mem);
+		break;
 	case HIO_HANDLE_TYPE_CALLBACKS: {
 		uint8 tmp[4];
 		int bytes_read = (* h->handle.callbacks->read)(h->handle.callbacks->user_data, &tmp, 1, 4);
@@ -351,6 +352,12 @@ long hio_tell(HIO_HANDLE *h)
 		ret = ftell(h->handle.file);
 		if (ret < 0) {
 			h->error = errno;
+		}
+		break;
+	case HIO_HANDLE_TYPE_MEMORY:
+		ret = mtell(h->handle.mem);
+		if (ret < 0) {
+			h->error = ret;
 		}
 		break;
 	case HIO_HANDLE_TYPE_CALLBACKS: {